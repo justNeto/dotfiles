045100d150304b20e88779c286b7c614b60691db
justNeto <continental.lelgg.2b@gmail.com>
2025-11-16 14:34:41 -0600

    Update .gitignore with explicit inclusion/exclusion rules

--- .config/mango/autostart.sh
+++
@@ -1,5 +0,0 @@
-waybar -c ~/.config/mango/config.jsonc -s ~/.config/mango/style.css >/dev/null 2>&1 &
-mako &
-swww-daemon &
-rwp &
-dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP &
--- .config/mango/rebar.sh
+++
@@ -1,1 +0,0 @@
-pkill waybar; waybar -c ~/.config/mango/config.jsonc -s ~/.config/mango/style.css >/dev/null 2>&1 &
